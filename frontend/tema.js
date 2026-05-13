7543e4f4d06066d7356b3ee70d2aee75ca8e4774
Milena Magaldi Moura <art0066385@aluno.firjansenaisesi.com.br>
2026-05-13 15:33:15 -0300

    mais imagens

--- frontend/tema.js
+++ frontend/tema.js
@@ -22,7 +22,12 @@ document.addEventListener('DOMContentLoaded', () => {
     if (splash && espacoBonequinho) {
         const imagensSplash = [
             '../misc/imgs/2_SplashScreen1.png',
-            '../misc/imgs/3_SplashScreen2.png'
+            '../misc/imgs/3_SplashScreen2.png',
+            '../misc/imgs/4_SplashScreen3.png',
+            '../misc/imgs/5_SplashScreen4.png',
+            '../misc/imgs/6_SplashScreen5.png',
+            '../misc/imgs/7_SplashScreen6.png',
+            '../misc/imgs/8_SplashScreen7.png'
         ];
         
         const imagemSorteada = imagensSplash[Math.floor(Math.random() * imagensSplash.length)];
